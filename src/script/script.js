3bdc979bc83f5a0554312e8184f8de1d6b0ddbe9
Jean Lucas <j3anlucas010@gmail.com>
2024-05-28 10:21:34 -0300

    Melhoria Na troca de fotos Do Perfil Principal

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,17 @@
-let trocar=document.querySelector('.foto')
-function trocarDefoto(){
-        trocar.addEventListener('click',function(){
-                trocar.classList.remove('foto')
-                trocar.classList.add('foto2')
-        })
+// Troca de Fotos Do Perfil Principal
+
+let trocar = document.querySelector(".foto");
+function trocarDefoto() {
+  trocar.addEventListener("click", function () {
+    trocar.classList.remove("foto");
+    trocar.classList.add("foto2");
+  });
+  function trocarDefoto1() {
+    trocar.addEventListener("mouseover", function () {
+      trocar.classList.remove("foto2");
+      trocar.classList.add("foto");
+    });
+  }
+  trocarDefoto1();
 }
-trocarDefoto()
+trocarDefoto();
